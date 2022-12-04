69e6de6aee0c1e8483cfbffaf94b89a9e9e9253b
SeongHanIm <tjdgksdl1120@gmail.com>
2022-12-04 22:03:28 +0900

    8주차 1~5번 문제

--- src/seonghan/bs/bs_12015.java
+++ src/seonghan/bs/bs_12015.java
@@ -1,2 +1,57 @@
-package seonghan.bs;public class bs_12015 {
+package seonghan.bs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class bs_12015 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] seq = new int[N];
+        int[] LIS = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+
+        LIS[0] = seq[0];
+        int lengthOfLIS = 1;
+
+        for (int i = 1; i < N; i++) {
+
+            int key = seq[i];
+
+            if (LIS[lengthOfLIS - 1] < key) {
+                lengthOfLIS++;
+                LIS[lengthOfLIS - 1] = key;
+            }
+            else {
+                int lo = 0;
+                int hi = lengthOfLIS;
+                while (lo < hi) {
+                    int mid = (lo + hi) >>> 1;
+
+                    if(LIS[mid] < key) {
+                        lo = mid + 1;
+                    }
+                    else {
+                        hi = mid;
+                    }
+
+                }
+                LIS[lo] = key;
+
+            }
+
+        }
+        System.out.println(lengthOfLIS);
+    }
 }
--- src/seonghan/bs/bs_2417.java
+++ src/seonghan/bs/bs_2417.java
@@ -1,2 +1,31 @@
-package seonghan.bs;public class bs_2417 {
+package seonghan.bs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bs_2417 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+        long start = 0;
+        long end = n;
+        long min = Long.MAX_VALUE;
+
+        while(start<=end){
+            long mid = (start + end)/2;
+
+            //Math.pow(밑,지수)
+            if(Math.pow(mid,2) >= n){
+                min = mid;
+                end = mid - 1;
+            }else{
+                start= mid + 1;
+            }
+        }
+
+        System.out.println(min);
+
+    }
 }
