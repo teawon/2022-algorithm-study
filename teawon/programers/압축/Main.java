da92821d916860058b8676e8ba8f527e14dd8cb9
teawon <xodnjsvkvk9@gmail.com>
2023-02-17 23:44:13 +0900

    18주차 프로그래머스 3번문제

---
+++ teawon/programers/압축/Main.java
@@ -0,0 +1,12 @@
+package teawon.programers.압축;
+
+public class Main {
+    public static void main(String[] args) {
+        String input1_1 = "kakao";
+        String input1_2 = "TOBEORNOTTOBEORTOBEORNOT";
+
+        System.out.println(Solution.solution(input1_1));
+
+    }
+
+}
